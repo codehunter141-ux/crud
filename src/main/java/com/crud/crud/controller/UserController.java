ef15f48005f823550b1d6bc561d353ec1dc94aa8
codehunter141-ux <codehunter141@gmail.com>
2026-01-13 16:58:56 +0530

    Update CrossOrigin URL in UserController

--- src/main/java/com/crud/crud/controller/UserController.java
+++ src/main/java/com/crud/crud/controller/UserController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@CrossOrigin(origins = "http://localhost:5173")
+@CrossOrigin(origins = "https://dxiox2zry0050.cloudfront.net")
 @RestController
 @RequestMapping("/api/rest")
 public class UserController {
